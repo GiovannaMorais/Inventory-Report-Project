baeb0ce5c2a5ce9f155e0df1aaaf9d6f6c450c19
GiovannaMorais <gigimorais9@hotmail.com>
2023-01-31 17:59:37 -0300

    Feat: Test from individual product report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    mock_product = Product(
+        6,
+        "Chocolate",
+        "Cacau Show",
+        "2022-12-31",
+        "2022-01-31",
+        "CACAU123",
+        "Na geladeira",
+    )
+
+    mock_relatorio_product = (
+        f"O produto {mock_product.nome_do_produto}"
+        f" fabricado em {mock_product.data_de_fabricacao}"
+        f" por {mock_product.nome_da_empresa} com validade"
+        f" até {mock_product.data_de_validade}"
+        f" precisa ser armazenado {mock_product.instrucoes_de_armazenamento}."
+    )
+
+    assert repr(mock_product) == mock_relatorio_product
