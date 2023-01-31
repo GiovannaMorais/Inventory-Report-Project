a6dd7a4f31ba87310bb06c613da87fc556b666e1
GiovannaMorais <gigimorais9@hotmail.com>
2023-01-31 16:57:18 -0300

    Feat: Generate reports through a JSON file

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,14 +1,21 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
     @classmethod
     def import_data(cls, file_path: str, file_type: str):
-        with open(file_path) as files:
-            reader = csv.DictReader(files)
-            list_file = list(reader)
+        if file_path.endswith('.csv'):
+            with open(file_path) as files:
+                reader = csv.DictReader(files)
+                list_file = list(reader)
+
+        if file_path.endswith('.json'):
+            with open(file_path) as files:
+                list_file = json.load(files)
+
         if file_type == 'simples':
             return SimpleReport.generate(list_file)
         if file_type == 'completo':
