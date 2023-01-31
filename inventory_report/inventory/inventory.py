d0651cf48c36b02716f935d17b5698a2c4219037
GiovannaMorais <gigimorais9@hotmail.com>
2023-01-31 17:36:27 -0300

    Feat: Generate reports through an XML file

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,9 +2,11 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
 import json
+import xmltodict
 
 
 class Inventory:
+
     @classmethod
     def import_data(cls, file_path: str, file_type: str):
         if file_path.endswith('.csv'):
@@ -12,9 +14,14 @@ class Inventory:
                 reader = csv.DictReader(files)
                 list_file = list(reader)
 
-        if file_path.endswith('.json'):
+        elif file_path.endswith('.json'):
             with open(file_path) as files:
                 list_file = json.load(files)
+
+        elif file_path.endswith(".xml"):
+            with open(file_path) as files:
+                reader = files.read()
+                list_file = xmltodict.parse(reader)["dataset"]["record"]
 
         if file_type == 'simples':
             return SimpleReport.generate(list_file)
