d5762863d5cbd22c8f686d51df26c183551c1b14
GiovannaMorais <gigimorais9@hotmail.com>
2023-01-31 19:45:59 -0300

    Feat: Populate the Main function

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,24 @@
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+
 def main():
-    pass
+    try:
+        _, file_path, file_type = sys.argv
+        result = str
+
+        if file_path.endswith('.csv'):
+            result = InventoryRefactor(CsvImporter)
+        elif file_path.endswith('.json'):
+            result = InventoryRefactor(JsonImporter)
+        else:
+            result = InventoryRefactor(XmlImporter)
+
+        inventory_report = result.import_data(file_path, file_type)
+
+        print(inventory_report, file=sys.stdout, end="")
+    except ValueError:
+        print("Verifique os argumentos", file=sys.stderr)
