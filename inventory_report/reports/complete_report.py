f8059a49d3c780b13f7bc4514ce6a93281702ce0
GiovannaMorais <gigimorais9@hotmail.com>
2023-01-31 16:02:47 -0300

    Feat: Generate the full version of the report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,25 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    def products_stocked_by_company(products: list):
+        company_list_str = ""
+        company_list = Counter(
+            [product["nome_da_empresa"] for product in products]
+        )
+        companies = dict(company_list)
+        for company in companies:
+            company_list_str += (
+                f"- {company}: {companies[company]}\n")
+        return company_list_str
+
+    @classmethod
+    def generate(cls, products):
+        simple_report = super().generate(products)
+        products_stocked = cls.products_stocked_by_company(products)
+        return (
+            f"{simple_report}\n"
+            "Produtos estocados por empresa:\n"
+            f"{products_stocked}"
+        )
